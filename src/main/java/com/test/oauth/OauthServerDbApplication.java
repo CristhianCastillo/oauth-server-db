4e8efe2d698fb03aef39ef21b13da99f0732e2d9
CristhianCastillo <cece9707@gmail.com>
2020-08-12 23:45:53 -0500

    OAuth con Token en la Base de datos
    Se agrega configuracion para persistir tokens en base de datos

---
+++ src/main/java/com/test/oauth/OauthServerDbApplication.java
@@ -0,0 +1,13 @@
+package com.test.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthServerDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthServerDbApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/test/oauth/security/AuthorizationServerConfig.java
@@ -0,0 +1,146 @@
+package com.test.oauth.security;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+import java.util.Calendar;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    private static final Logger log = LogManager.getLogger(AuthorizationServerConfig.class);
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserDetailsService userService;
+
+    @Autowired
+    private InformationToken informationToken;
+
+    /**
+     * Save tokens in database
+     */
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Value("${jwt.clientId:kentaurus}")
+    private String clientId;
+
+    @Value("${jwt.client-secret:secret}")
+    private String clientSecret;
+
+    @Value("${jwt.accessTokenValidititySeconds:43200}") // 12 hours
+    private int accessTokenValiditySeconds;
+
+    @Value("${jwt.authorizedGrantTypes:password,authorization_code,refresh_token}")
+    private String[] authorizedGrantTypes;
+
+    @Value("${jwt.refreshTokenValiditySeconds:2592000}") // 30 days
+    private int refreshTokenValiditySeconds;
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        log.debug("##### OAUTH ##### " + Calendar.getInstance().getTimeInMillis() + " - Configuring Security Autorization Server");
+        security
+                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()")
+                .allowFormAuthenticationForClients();
+    }
+
+//    @Override
+//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//        log.debug("##### OAUTH ##### " + Calendar.getInstance().getTimeInMillis() + " - Configuring Client Details Service");
+//        InMemoryClientDetailsServiceBuilder builder = clients.inMemory();
+//        builder.withClient("test-app")
+//                .secret(passwordEncoder.encode("12345"))
+//                .scopes("read", "write")
+//                .authorizedGrantTypes("password", "refresh_token")
+//                .accessTokenValiditySeconds(43200)
+//                .refreshTokenValiditySeconds(2592000)
+//                .and();
+//    }
+
+    /**
+     * Configuration for save tokens in database
+     *
+     * @param configurer
+     * @throws Exception
+     */
+    @Override
+    public void configure(ClientDetailsServiceConfigurer configurer) throws Exception {
+        configurer.jdbc(jdbcTemplate.getDataSource());
+    }
+
+    @Override
+    public void configure(final AuthorizationServerEndpointsConfigurer endpoints) {
+        log.debug("##### OAUTH ##### " + Calendar.getInstance().getTimeInMillis() + " - Configuring Endpoints Autorization Server");
+
+
+//        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+//        tokenEnhancerChain.setTokenEnhancers(Arrays.asList(accessTokenConverter(), informationToken));
+
+        endpoints.authenticationManager(authenticationManager)
+                .tokenStore(tokenStore()) // For save token in memory
+                .accessTokenConverter(accessTokenConverter())
+                .userDetailsService(userService);
+//                .tokenEnhancer(tokenEnhancerChain);
+    }
+
+    /**
+     * Configuration for save tokens in database
+     *
+     * @return
+     */
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(jdbcTemplate.getDataSource());
+    }
+
+    /**
+     * Configuration for save tokens in database.
+     * Making this primary to avoid any accidental duplication with another token service instance of the same name.
+     *
+     * @return
+     */
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        defaultTokenServices.setSupportRefreshToken(true);
+        return defaultTokenServices;
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        log.debug("##### OAUTH ##### " + Calendar.getInstance().getTimeInMillis() + " - Creating Access Token Converter");
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("**aze6z9i8d^=d.*GLM4AXh)Hpbg3M]sR4f7cxD!:9e6THrPtp^!AY=&FFUm7kK^");
+        return converter;
+    }
+
+}
